b2ea5b8174e878f6d5af9797c8b8d73fad23b265
HungKM <hungkm@HungKM-PC.(none)>
2012-07-30 11:48:54 +0900

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 ChatServer::Application.routes.draw do
-  match '/signup', to: "users#new"
+  resources :users
 
   root to: 'static_pages#home'
+
+  match '/signup', to: "users#new"
   match '/help', to: 'static_pages#help'
   match '/about', to: 'static_pages#about'
   match '/contact', to: 'static_pages#contact'
---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name "Micheal Hartl"
+    email "micheal@example.com"
+    password "foobar"
+    password_confirmation "foobar"
+  end
+end
