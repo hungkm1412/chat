7000f0667989e3a18b2e1b0f436abeceb72938f4
HungKM <hungkm@HungKM-PC.(none)>
2012-08-19 11:54:42 +0900

    Add user following

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -7,4 +7,8 @@ class Micropost < ActiveRecord::Base
   validates :content, presence: true, length: { maximum: 140 }
 
   default_scope order: 'microposts.created_at DESC'
+
+  def self.from_users_followed_by(user)
+    where("user_id IN (?) OR user_id = ? ", user.followed_user_ids, user)
+  end
 end
